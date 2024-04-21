20f01544f519d241d3d399f0164e04bcec918d6e
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2024-04-21 22:42:10 +0700

    Menghapus Comment yang tidak diperlukan
    Menghapus Comment yang tidak diperlukan pada class Employee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -51,10 +51,6 @@ public class Employee {
 		childIdNumbers = new LinkedList<String>();
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
 	
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
@@ -95,7 +91,6 @@ public class Employee {
 	
 	public int getAnnualIncomeTax() {
 		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
 		if (date.getYear() == yearJoined) {
