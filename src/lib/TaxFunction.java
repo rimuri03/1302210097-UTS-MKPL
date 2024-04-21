a9da39e8129566b105252013b59273ac40714b32
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2024-04-21 23:59:20 +0700

    Menghilangkan magic numbers dari kelas TaxFunction
    Menghilangkan magic numbers dari kelas TaxFunction

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -2,32 +2,28 @@ package lib;
 
 public class TaxFunction {
 
-	
-	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+    private static final int STANDARD_DEDUCTIBLE = 54000000;
+    private static final int PER_CHILD_DEDUCTION = 1500000;
+    private static final int MAX_CHILDREN = 3;
+    private static final double TAX_RATE = 0.05;
+
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
+            boolean isMarried, int numberOfChildren) {
+        if (numberOfMonthWorking > 12) {
+            System.err.println("More than 12 months working per year");
+            return 0;
+        }
+        numberOfChildren = Math.min(numberOfChildren, MAX_CHILDREN);
+
+        int totalIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
+
+        int totalDeductible = deductible + STANDARD_DEDUCTIBLE + (numberOfChildren * PER_CHILD_DEDUCTION);
 		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
-	
+        if (!isMarried) {
+            totalDeductible -= PER_CHILD_DEDUCTION;
+        }
+        int tax = (int) Math.round(TAX_RATE * (totalIncome - totalDeductible));
+
+        return Math.max(tax, 0);
+    }
 }
